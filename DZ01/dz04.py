a0a835051c43d302b34ad827b3382d171dc350d7
bernazsg <ser-bernaz@mail.ru>
2021-07-22 21:06:09 +0300

    modify dz3 -4

--- DZ01/dz04.py
+++ DZ01/dz04.py
@@ -1,11 +1,11 @@
-tceloe_chislo = input("Введите целое положительное число >>> ")
-kolichestvo_cifr = len(tceloe_chislo) - 1
-index = 0
-while kolichestvo_cifr > index:
-    if int(tceloe_chislo[index]) > int(tceloe_chislo[index + 1]):
-        naibolshee = int(tceloe_chislo[index])
-    else:
-        naibolshee = int(tceloe_chislo[index + 1])
-    index += 1
-print("Самая большая цифра в вашем числе", naibolshee)
+# tceloe_chislo = input("Введите целое положительное число >>> ")
+# kolichestvo_cifr = len(tceloe_chislo) - 1
+# index = 0
+# while kolichestvo_cifr > index:
+#     if int(tceloe_chislo[index]) > int(tceloe_chislo[index + 1]):
+#         naibolshee = int(tceloe_chislo[index])
+#     else:
+#         naibolshee = int(tceloe_chislo[index + 1])
+#     index += 1
+# print("Самая большая цифра в вашем числе", naibolshee)
 
