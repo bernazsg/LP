21f1770bd0227ced4dc6e8fd22dcfe91c8f29925
bernazsg <ser-bernaz@mail.ru>
2021-07-22 21:29:19 +0300

    modify dz04.py

--- DZ01/dz04.py
+++ DZ01/dz04.py
@@ -1,3 +1,25 @@
+# Пользователь вводит целое положительное число. Найдите самую большую цифру в числе.
+# Для решения используйте цикл while и арифметические операции.
+while True:
+    tceloe = input("Введите целое положительное число >>> ")
+    if not str.isdigit(tceloe):
+        print("Введён неверный формат данных - дробное, отрицательное число или символ. Попробуйте снова.")
+        continue
+    else:
+        print(f"Будет найдена самая большая цифра в числе {tceloe}")
+        break
+
+tceloe = int(tceloe)
+naibolsh = 0
+while tceloe >= 1:
+    ostatok = tceloe % 10
+    tceloe //= 10
+    if naibolsh < ostatok:
+        naibolsh = ostatok
+
+print(f"Наибольшая цифра во введённом числе - {naibolsh}.")
+
+# Альтернативное решение:
 # tceloe_chislo = input("Введите целое положительное число >>> ")
 # kolichestvo_cifr = len(tceloe_chislo) - 1
 # index = 0
@@ -8,4 +30,3 @@
 #         naibolshee = int(tceloe_chislo[index + 1])
 #     index += 1
 # print("Самая большая цифра в вашем числе", naibolshee)
-
