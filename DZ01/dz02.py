94bc02ee3cdeee8ddf58e3a04942511c2665da60
bernazsg <ser-bernaz@mail.ru>
2021-07-22 20:50:10 +0300

    dz01_modify

--- DZ01/dz02.py
+++ DZ01/dz02.py
@@ -1,13 +1,19 @@
-import datetime
-
-time_sec = int(input("Введите время в секундах (число не более 86400) >>> "))
-if time_sec > 86400:
-    print("Заданное вами число превышает указанный предел в 86400 секунд.")
-else:
-    hour_time = time_sec // 3600
-    hour_ostatok = time_sec % 3600
-    min_time = hour_ostatok // 60
-    sec_time = hour_ostatok % 60
-
-    time_res = datetime.time(hour_time, min_time, sec_time)
-    print(f"Вычисленное время: {time_res}")
+# Пользователь вводит время в секундах. Переведите время в часы, минуты и секунды и выведите в формате чч:мм:сс.
+# Используйте форматирование строк.
+while True:
+    time_sec = input("Введите целое число в секундах,но не более 356519 >>> ")
+    if not str.isdigit(time_sec):
+        print(
+            "Введённый форма данных неверен, вы ввели дробное число, отрицательное число или символ. Попробуйте снова")
+        continue
+    if int(time_sec) > 356519:
+        print("Вы ввели число более 356519. Попробуйте снова.")
+        continue
+    else:
+        time_sec = int(time_sec)
+        hour_time = time_sec // 3600
+        ostatok = time_sec % 3600
+        time_min = ostatok // 60
+        sec_time = ostatok % 60
+        break
+print(f"Рассчитанное время: {hour_time:<02}:{time_min:<02}:{sec_time:<02}.")
